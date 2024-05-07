e7f87b410ff5f8e951b563cc67d4b4f1ad3fb5e8
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-06 18:14:18 -0600

    refactor: :recycle: modify useForm for validation  in register

--- src/shared/hooks/useForm.js
+++ src/shared/hooks/useForm.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, paramsFormValidations = {}) => {
 
     const [formState, setFormState] = useState(initialForm);
+    const [formValidation, setFormValidation] = useState({})
+
+    useEffect(() => {
+      createValidator()
+    }, [formState])
+    
 
     const onInputChange = ( { target }) => {
         const { name, value} = target;
@@ -17,11 +23,41 @@ export const useForm = (initialForm = {}) => {
         setFormState( initialForm )
     }
 
+/**
+ * Meotodo para validar el formulario 
+ * con base a el objetop a evaluar. 
+ * si cumple la validacion el mensaje sera nulo 
+ * de lo contrario  mandara el mensaje de error establecido.
+ */
+    const createValidator = () => { 
+
+      const formCheckedValues = {};
+
+      /**
+       * para recorrer el formulario de la validacion imprime sus campos:
+       * email, password, displyName, etc...
+       */
+      for( const formField of Object.keys(paramsFormValidations)) { 
+        // console.log(formField);
+        const [method, errorMessage  = 'este campo es requerido '] = paramsFormValidations[formField]; 
+
+        /**
+         * para mostrar mensaje de error si existe un valor o es correcto la validacion manda un null
+         * de lo contrario manda el error message
+         */
+        formCheckedValues[`${ formField }Valid`] = method(formState[formField]) ? null : errorMessage;
+
+        setFormValidation(formCheckedValues);
+      }
+    }
+
 
   return {
     ...formState,
     formState,
     onInputChange,
-    onResetForm
+    onResetForm,
+
+    ...formValidation
   }
 }
