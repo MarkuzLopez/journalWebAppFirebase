e29f4321876ab98b88e059579c023f8f4a6256bf
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-08 11:19:47 -0600

    refactor: :zap: custom hook for check authentication
    cut coding of app routes for create new hook as a whole more readable.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,27 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { useDispatch, useSelector } from 'react-redux'
 import { CheckingAuth } from '../ui/components/CheckingAuth'
-import { onAuthStateChanged } from 'firebase/auth'
-import { firebaseAuth } from '../firebase/config'
-import { login } from '../store/auth/authSlice'
+import { useCheckAuth } from '../shared/hooks/useCheckAuth'
 
 export const AppRouter = () => {
 
-  const { status} = useSelector( state =>  state.auth );
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-   onAuthStateChanged(firebaseAuth,  async (user) => { 
-    if(!user) return
-
-    const { uid,  displayName , email, photoURL } = user;
-    dispatch(login({uid,  displayName , email, photoURL}));
-   })
-  }, [])
-  
+  const status  =  useCheckAuth()
 
   if(status === 'checking') {
    return  <CheckingAuth /> 
---
+++ src/shared/hooks/useCheckAuth.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { firebaseAuth } from '../../firebase/config';
+import { login } from '../../store/auth/authSlice';
+
+export const useCheckAuth = () => {
+
+    const { status} = useSelector( state =>  state.auth );
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+     onAuthStateChanged(firebaseAuth,  async (user) => { 
+      if(!user) return
+  
+      const { uid,  displayName , email, photoURL } = user;
+      dispatch(login({uid,  displayName , email, photoURL}));
+     })
+    }, [])
+
+    return status
+}
