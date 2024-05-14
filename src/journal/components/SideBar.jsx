04fa2f0e5ba5125bb360770909f97331ecf1e6b8
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-14 13:15:53 -0600

    perf: :zap: sidebar show notes
    regorganization of item en sidebar for new component

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,11 +1,12 @@
-import { TurnedInNot } from '@mui/icons-material'
-import { Box, Divider, Drawer, Grid, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material'
 import { useSelector } from 'react-redux'
+import { SideBarItem } from './SideBarItem'
 
 export const SideBar = ({ drawerWidth= 240 }) => {
 
     const { displayName } = useSelector(state => state.auth);
+    const { notes } = useSelector(state => state.journal);
 
   return (
     <Box 
@@ -28,17 +29,8 @@ export const SideBar = ({ drawerWidth= 240 }) => {
 
         <List>
             {
-                ['Lunes', 'Martes', 'Miercoles', 'Jueves'].map(text => (
-                    <ListItemButton key={text} >
-                        <ListItemIcon>
-                            <TurnedInNot />
-                        </ListItemIcon>
-
-                        <Grid container>
-                            <ListItemText primary={text} />
-                            <ListItemText secondary={'asdasdasdas adsadasdasda asdasdadasdasd'} />
-                        </Grid>               
-                    </ListItemButton>
+                notes.map(note => (
+                  <SideBarItem  key={note.id} note={note} />
                 ))
             }
         </List>
