c21d87fcc17eff5caeb95daf7dda210e403ac828
MarkuzLopez <marco.02lopez@hotmail.com>
2024-05-24 11:40:35 -0600

    test: :white_check_mark: unit test authslice

--- src/store/auth/tests/authSlice.test.js
+++ src/store/auth/tests/authSlice.test.js
@@ -1,14 +1,52 @@
-import { authSlice } from "../authSlice"
-import { initialState } from "./fixtures/authFixtures";
+import { authSlice, checkingCredentials, login, logOut } from "../authSlice";
+import {
+  demoUser,
+  initialState,
+  notAuthenticated,
+} from "./fixtures/authFixtures";
 
-describe('Pruebas authSlice', () => { 
+describe("Pruebas authSlice", () => {
+  test("debe de regresar el estado inicial y llamarse auth", () => {
+    // console.log(authSlice);
+    expect(authSlice.name).toBe("auth");
 
-    test('debe de regresar el estado inicial y llamarse auth', () => { 
-        
-        // console.log(authSlice);
-        expect( authSlice.name ).toBe('auth');
+    expect(authSlice.reducer(initialState, {})).toEqual(initialState);
+  });
 
-        expect( authSlice.reducer(initialState, {}) ).toEqual(initialState);
-     })
+  test("debe de realizar la autenticacion", () => {
+    const state = authSlice.reducer(initialState, login(demoUser));
 
- })
+    expect(state).toEqual({
+      status: "authenticated",
+      uid: demoUser.uid,
+      email: demoUser.email,
+      displayName: demoUser.displayName,
+      photoURL: demoUser.photoURL,
+      errorMessage: null,
+    });
+  });
+
+  test("debe de realizar el logOut", () => {
+    const state = authSlice.reducer(initialState, logOut(notAuthenticated));
+    expect(state).toEqual({
+      status: "not-authenticated",
+      email: null,
+      displayName: null,
+      photoURL: null,
+      uid: null,
+      errorMessage: null,
+    });
+  });
+
+  test("debe de realizar el chequeo de credenciales", () => {
+    const state = authSlice.reducer(initialState, checkingCredentials());
+    expect(state).toEqual({
+      status: "checking",
+      email: null,
+      displayName: null,
+      photoURL: null,
+      uid: null,
+      errorMessage: null,
+    });
+  });
+});
